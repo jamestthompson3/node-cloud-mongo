b777d8e01eca7bee1bda63d26d2fc900ebfb4a72
nangelos <nicksangelos@gmail.com>
2019-01-08 13:57:05 -0600

    [auth.js] Cookies set for new user registration

--- src/front/src/common/api/auth.js
+++ src/front/src/common/api/auth.js
@@ -5,17 +5,18 @@ export const signUp = ({ email, password }) =>
   request
     .post('/api/auth/register')
     .send({ email, password })
-    .then(res => Cookies.set('AuthToken', res.body.user.token))
+    .then(res => {
+      if (res.statusCode === 200) {
+        Cookies.set('AuthToken', res.body.user.token)
+      }
+    })
 
 export const login = ({ username, password }) =>
   request
     .post('/api/auth/login/')
     .send({ username, password })
     .then(res => {
-      console.log(res)
-
       if (res.statusCode === 200) {
-        console.log('Code 200')
         Cookies.set('AuthToken', res.body.user.token)
       }
     })
