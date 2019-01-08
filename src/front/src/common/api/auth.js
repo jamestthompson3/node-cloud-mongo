6a8ebdc20d9fbdbeb42e159189d2b933d87743b1
nangelos <nicksangelos@gmail.com>
2019-01-08 13:51:26 -0600

    [auth.js] Cookies being set for login

--- src/front/src/common/api/auth.js
+++ src/front/src/common/api/auth.js
@@ -1,7 +1,21 @@
 import request from 'superagent'
+import Cookies from 'js-cookie'
 
 export const signUp = ({ email, password }) =>
-  request.post('/api/auth/register').send({ email, password })
+  request
+    .post('/api/auth/register')
+    .send({ email, password })
+    .then(res => Cookies.set('AuthToken', res.body.user.token))
 
 export const login = ({ username, password }) =>
-  request.post('/api/auth/login/').send({ username, password })
+  request
+    .post('/api/auth/login/')
+    .send({ username, password })
+    .then(res => {
+      console.log(res)
+
+      if (res.statusCode === 200) {
+        console.log('Code 200')
+        Cookies.set('AuthToken', res.body.user.token)
+      }
+    })
